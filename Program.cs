50d45c4527b85fde22d0d3ae194a052214635678
Akira Ichikawa <akira.ichikawa@sportserve.co>
2026-02-07 15:44:03 -0300

    branch test

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 /*1. Saudação com Nome 
 Objetivo: Declarar variáveis, usar concatenação e interpolação. 
-Descrição: Crie um script que declare uma variável para o nome de 
+Descrição: Crie uasdasdasdasdasdm script que declare uma variável para o nome de 
 uma pessoa e exiba uma saudação personalizada.
 */
-
+asdasd
 Console.WriteLine("\n=== Exercício 1 ===\n");
 
 string nomePessoa = "João Silva";
